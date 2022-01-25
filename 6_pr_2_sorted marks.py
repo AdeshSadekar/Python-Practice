54a7c610b6eb8153a62d7756e927c486440056c1
AdeshSadekar <73415142+AdeshSadekar@users.noreply.github.com>
2022-01-25 22:06:11 +0530

    Add files via upload

---
+++ 6_pr_2_sorted marks.py
@@ -0,0 +1,11 @@
+m1 = int(input("Enter marks number 1 :"))
+m2 = int(input("Enter marks number 2 :"))
+m3 = int(input("Enter marks number 3 :"))
+m4 = int(input("Enter marks number 4 :"))
+m5 = int(input("Enter marks number 5 :"))
+m6 = int(input("Enter marks number 6 :"))
+m7 = int(input("Enter marks number 7 :"))
+
+MyMarkList = [m1,m2,m3,m4,m5,m6,m7]
+MyMarkList.sort()
+print(MyMarkList)
---
+++ 8_stringfunctions.py
@@ -0,0 +1,8 @@
+story = "Once upon a time there was a youtuber named harry who uploaded python course with notes Harry"
+
+print(len(story))
+print(story.endswith("notes"))
+print(story.count("u"))
+print(story.capitalize())
+print(story.find("upon"))
+print(story.replace("Harry", "CodewithHarry"))
