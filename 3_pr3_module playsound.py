a271d15e2e5e843a03d6108c1f1c921f752b7453
AdeshSadekar <73415142+AdeshSadekar@users.noreply.github.com>
2022-01-25 22:02:33 +0530

    Add files via upload

---
+++ 3_pr3_module playsound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('C:\\Users\\hp\\Desktop\\Adesh College\\Python Practise\\mp3song.mp3')
---
+++ 4_pr_4_listdirectory.py
@@ -0,0 +1,6 @@
+# import os
+# print(os.listdir())
+
+a = input("Enter a number: ")
+a = int(a)
+print(type(a))
